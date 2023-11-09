dc7b602915b9d55a63bf1eb031e75f767d1b480c
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-09 11:25:22 +0530

    composed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,75 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import RestaurantList from './components/RestaurantList/RestaurantList';
+import RestaurantViewer from './components/RestaurantViewer/RestaurantViewer';
+import ErrorBoundry from './components/ErrorBoundry';
+import { fetchHelper } from './components/RestaurantViewer/helpers/fetch';
+import { HOSTNAME } from './config';
+
+export type RestaurantPreviewData = {
+  name: string;
+  rating: number;
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  contacts: {
+    phone: string;
+    email: string;
+  };
+  links: {
+    view: string;
+  };
+};
 
 function App() {
-  return <></>;
+  const [restaurants, setRestaurants] = useState<RestaurantPreviewData[]>([]);
+  const [previewURL, setPreviewURL] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const url = HOSTNAME + '/restaurants';
+        const config: RequestInit = {
+          method: 'GET',
+        };
+        const result = await fetchHelper(url, config);
+        if (result) {
+          setRestaurants(result.message);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    return () => {};
+  }, []);
+
+  function setRestaurantLink(url: string) {
+    setPreviewURL(url);
+  }
+
+  return (
+    <main
+      className={
+        'w-full max-w-screen-lg h-screen mx-auto flex flex-row items-stretch justify-start gap-[1rem]'
+      }>
+      <div className={'flex-[1] w-full h-full'}>
+        <ErrorBoundry>
+          <RestaurantList
+            restaurants={restaurants}
+            setRestaurant={setRestaurantLink}
+          />
+        </ErrorBoundry>
+      </div>
+      <div className={'flex-[3] w-full max-h-screen overflow-y-auto'}>
+        <ErrorBoundry>
+          <RestaurantViewer URL={previewURL} />
+        </ErrorBoundry>
+      </div>
+    </main>
+  );
 }
 
 export default App;
